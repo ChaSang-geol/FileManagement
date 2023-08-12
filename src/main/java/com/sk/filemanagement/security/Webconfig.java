1c2995f45288b64b4a72a410ece2080a76b7e4be
ChaSang-geol <hunipapa@nate.com>
2023-08-13 00:54:13 +0900

    20230813

---
+++ src/main/java/com/sk/filemanagement/security/Webconfig.java
@@ -0,0 +1,16 @@
+package com.sk.filemanagement.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Webconfig implements WebMvcConfigurer {
+  public static final String ALLOWED_METHOD_NAMES = "GET,POST,PUT,DELETE,PATCH";
+
+  @Override
+  public void addCorsMappings(final CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
+  }
+}
